5c11a6a635d757624a1dae4f2ba8db24bf6025a7
ItsWil23 <homawoowilfried@gmail.com>
2022-03-20 19:28:13 +0100

    Ping

--- Ping/ping.py
+++ Ping/ping.py
@@ -6,6 +6,9 @@ class PingCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    async def cog_check(self, ctx):
+        return ctx.author.id == 737084050753323119
+
     @commands.command()
     async def ping(self, ctx):
         await ctx.message.reply(f'Pong!\n{round(self.bot.latency * 1000)}ms')
